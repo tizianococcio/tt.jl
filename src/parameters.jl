f7e888053917f152f0c39816aa5afe820586e309
Tiziano Cocciò <tiziano.coccio@gmail.com>
2022-05-24 21:57:56 +0200

    use triplet params from LKD

--- src/parameters.jl
+++ src/parameters.jl
@@ -2,7 +2,7 @@ using Parameters
 ms = 1
 
 # As in LKD paper
-#= @with_kw struct TripletSTDP
+@with_kw struct TripletSTDP
     tau_plus::Float32 = 16.8ms      # time constant representing decrease rate of presynaptic detector r1
     tau_minus::Float32 = 33.7ms     # time constant representing decrease rate of postsynaptic detector o1
     tau_x = 101ms                   # time constant representing decrease rate of presynaptic detector r2
@@ -11,10 +11,10 @@ ms = 1
     A_plus_3 = 9.3e-3
     A_minus_2 = 7e-3
     A_minus_3 = 2.3e-4
-end =#
+end
 
 # from Alessio
-@with_kw struct TripletSTDP
+#= @with_kw struct TripletSTDP
     tau_plus::Float32 = 16.8ms      # time constant representing decrease rate of presynaptic detector r1
     tau_minus::Float32 = 33.7ms     # time constant representing decrease rate of postsynaptic detector o1
     tau_x = 1ms                   # time constant representing decrease rate of presynaptic detector r2
@@ -23,7 +23,7 @@ end =#
     A_plus_3 = 6.5e-3
     A_minus_2 = 7.1e-3
     A_minus_3 = 0 
-end
+end =#
 
 @with_kw struct VoltageSTDP 
     dummy = "This is a dummy struct to facilatate use of multiple dispatch. The actual params are in sim()."
