8c26aba7ba1ed7d1aea669153d8a1c0bb0e30fe4
Tiziano Cocciò <tiziano.coccio@gmail.com>
2022-07-14 14:27:27 +0200

    implement JSD proximity computation

--- src/analysis/dynamics.jl
+++ src/analysis/dynamics.jl
@@ -23,7 +23,7 @@ Base.length(S::WeightTrace) = S.count
 
 """
 Computes KL-divergence of two weight traces
-1. Between the first init weights and the weights at time test (first_vs_t)
+1. Between the first init weights and the weights at time t (first_vs_t)
 2. Between the weight at time t and the final weight (t_vs_end)
 
 trace: the weight trace as returned by tt.get_weight_traces()
@@ -61,3 +61,76 @@ function compute_klds(trace::WeightTrace, n)
     end
     first_vs_t, t_vs_end
 end
+
+"""
+version allowing for the weights to be normalized beforehand and to choose the point of "end"
+"""
+function compute_klds(trace::WeightTrace, n, normalize = false, end_id = 0)
+    nrmlz(A) = A[:]/sum(A[:])
+    t = trace.count
+    _start = trace[1][1:n,1:n][:]
+    _end = trace[trace.count][1:n,1:n][:]
+    if end_id > 0
+        _end = trace[end_id][1:n,1:n][:]
+    end
+    if normalize
+        _start = nrmlz(_start)
+        _end = nrmlz(_end)
+    end
+    first_vs_t = Vector{Float64}(undef, t)
+    t_vs_end = Vector{Float64}(undef, t)
+    if normalize
+        for (i, w) in enumerate(trace)
+            first_vs_t[i] = kldivergence(_start, nrmlz(w[1:n,1:n][:]))
+            t_vs_end[i] = kldivergence(nrmlz(w[1:n,1:n][:]), _end)
+        end
+    else
+        for (i, w) in enumerate(trace)
+            first_vs_t[i] = kldivergence(_start, w[1:n,1:n][:])
+            t_vs_end[i] = kldivergence(w[1:n,1:n][:], _end)
+        end
+    end
+    first_vs_t, t_vs_end
+end
+
+using StatsBase
+
+# Jensen-Shannon divergence
+function jsd(P,Q)
+    M = 0.5*(P + Q)
+    0.5*kldivergence(P, M) + 0.5*kldivergence(Q, M)
+end
+
+function shuffle_proximity(initial_weights, final_weights)
+    @assert length(initial_weights) == length(final_weights)
+    N = length(initial_weights)
+    JD0 = 1/N * sum(jsd.(initial_weights, final_weights))
+    JDnm = zeros(N,N)
+    for n in eachindex(final_weights)
+        for m in eachindex(final_weights)
+            JDnm[n,m] = jsd(final_weights[n], final_weights[m]) / JD0
+        end
+    end
+    JDnm
+end 
+
+# nrmlz(A) = A[:]/sum(A[:])
+# A = rand(10,10)
+# B = rand(10,10)
+# Aend = copy(A)
+# Bend = copy(B)
+
+# start = [nrmlz(A),nrmlz(B)]
+# finish = [nrmlz(Aend), nrmlz(Bend)]
+# shuffle_proximity(start, finish)
+
+# # make them a bit different
+# Aend[2,:] .+= 0.01
+# Bend[3,:] .+= 0.01
+# Bend[4,:] .+= 0.01
+# Bend[5,:] .+= 0.01
+
+# Aend = rand(10,10)
+# Bend = rand(10,10)
+# finish = [nrmlz(Aend), nrmlz(Bend)]
+# shuffle_proximity(start, finish)
